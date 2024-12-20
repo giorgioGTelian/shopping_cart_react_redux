0b05f5877d79384177bc5cbfec60d7a60ee3d29d
giorgio <giorgio.programmer@gmail.com>
2024-12-20 14:18:27 +0000

    fixed missed imports

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,8 @@
-import React, { useState,useEffect } from 'react';
-import './ProductList.css'
+import React, { useState } from 'react';
+import './ProductList.css';
 import CartItem from './CartItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from './CartSlice';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
